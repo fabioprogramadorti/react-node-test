370bd9cb59a40c75f3ad99219004b08940f373f2
Fabio Santos <fabioprogramadorti@gmail.com>
2021-01-10 11:05:48 -0300

    Add promisse library

--- server/startServer.js
+++ server/startServer.js
@@ -1,12 +1,22 @@
 import { ApolloServer } from 'apollo-server'
 import mongoose from 'mongoose'
+import bluebird from 'bluebird'
 
+const connectionUri = 'mongodb://localhost:27017/graphql'
+const options = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		promiseLibrary: bluebird
+	}
 function startServer({ typeDefs, resolvers }){
 
-	mongoose.connect('mongodb://localhost:27017/graphql', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
+	mongoose.connect(connectionUri, options)
+		.then(res => {
+			console.log('MongoDB connected at: ' + connectionUri)
+		})
+		.catch(err => {
+			console.log(err)
+		})
 
 	const server = new ApolloServer({ typeDefs, resolvers })
 	server.listen().then(({ url }) => 
